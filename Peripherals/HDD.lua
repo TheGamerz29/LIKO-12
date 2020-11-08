adcc54f03feafcede5bee037833f03005d765074
Rami Sabbagh <ramilego4game@gmail.com>
2020-11-08 22:14:53 +0200

    I think writing 10 lines counts

--- Peripherals/HDD.lua
+++ Peripherals/HDD.lua
@@ -5,6 +5,18 @@ local class = require("libraries.middleclass")
 
 local HDD = class("peripherals.HDD")
 
+HDD.static.defaultProperties = {
+    rootPath = "/storage/main/", --must end with a trailing /
+    usageLimit = 20*1024*1024
+}
 
+function HDD:initialize(properties)
+    properties = properties or {}
+    --So 'nil' fields fallback to the default values.
+    setmetatable(properties, {__index = self.defaultProperties})
+
+    self.rootPath = properties.rootPath
+    self.usageLimit = properties.usageLimit
+end
 
 return HDD
