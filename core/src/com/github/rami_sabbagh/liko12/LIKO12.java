6c85787421d7a14a2586808715a4ff21858c3d64
Rami Sabbagh <ramilego4game@gmail.com>
2020-11-14 22:58:20 +0200

    Playing around with LibGDX

--- core/src/com/github/rami_sabbagh/liko12/LIKO12.java
+++ core/src/com/github/rami_sabbagh/liko12/LIKO12.java
@@ -1,33 +1,74 @@
 package com.github.rami_sabbagh.liko12;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class LIKO12 extends ApplicationAdapter {
+	OrthographicCamera camera;
+	ShapeRenderer shapeRenderer;
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false);
+
+		shapeRenderer = new ShapeRenderer();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("icon-square.png");
+
+		font = new BitmapFont();
+
+		Gdx.app.log("Lifecycle", "create");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello World", 5, 20);
 		batch.end();
+
+
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(1,1,1,1);
+
+		for (int pointer=0; pointer<Gdx.input.getMaxPointers(); pointer++) {
+			if (Gdx.input.isTouched(pointer) || Gdx.app.getType() == Application.ApplicationType.Desktop) {
+				shapeRenderer.circle(
+						Gdx.input.getX(pointer),
+						Gdx.graphics.getHeight() - Gdx.input.getY(pointer),
+						getDensity()*10);
+			}
+		}
+		shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
+		shapeRenderer.dispose();
 		batch.dispose();
 		img.dispose();
+	}
+
+	public float getDensity() {
+		if (Gdx.app.getType() == Application.ApplicationType.Desktop)
+			return 1.0f;
+		else
+			return Gdx.graphics.getDensity();
 	}
 }
--- desktop/src/com/github/rami_sabbagh/liko12/desktop/DesktopLauncher.java
+++ desktop/src/com/github/rami_sabbagh/liko12/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.github.rami_sabbagh.liko12.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.rami_sabbagh.liko12.LIKO12;
@@ -7,6 +8,10 @@ import com.github.rami_sabbagh.liko12.LIKO12;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "LIKO-12";
+		config.width = 192*5;
+		config.height = 128*5;
+		config.addIcon("icon-16x16.png", Files.FileType.Internal);
 		new LwjglApplication(new LIKO12(), config);
 	}
 }
