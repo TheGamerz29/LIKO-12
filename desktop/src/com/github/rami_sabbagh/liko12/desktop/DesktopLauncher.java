45bafb0b7b46e79e1246bce0963a3c2c31e87109
Rami Sabbagh <ramilego4game@gmail.com>
2020-12-05 14:16:15 +0200

    Upgraded to LWJGL3. As always, screw Apple.

--- desktop/src/com/github/rami_sabbagh/liko12/desktop/DesktopLauncher.java
+++ desktop/src/com/github/rami_sabbagh/liko12/desktop/DesktopLauncher.java
@@ -1,23 +1,20 @@
 package com.github.rami_sabbagh.liko12.desktop;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.github.rami_sabbagh.liko12.LIKO12;
 
-public class DesktopLauncher {
-	private static final int LIKO_SCALE = 5;
+import static com.badlogic.gdx.Files.FileType.Internal;
 
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "LIKO-12";
-		config.x = 250;
-		config.y = 70;
-		config.width = 192 * LIKO_SCALE;
-		config.height = 128 * LIKO_SCALE;
-		config.resizable = true;
-		config.addIcon("icon-16x16.png", Files.FileType.Internal);
-		new LwjglApplication(new LIKO12(), config);
-	}
+public class DesktopLauncher {
+    private static final int LIKO_SCALE = 5;
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("LIKO-12");
+        config.setWindowedMode(192 * LIKO_SCALE, 128 * LIKO_SCALE);
+        config.setResizable(true);
+        config.setWindowIcon(Internal, "icon-16x16.png");
+        new Lwjgl3Application(new LIKO12(), config);
+    }
 }
